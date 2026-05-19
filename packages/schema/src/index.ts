967b058312b2225a8bc43012486d5717161d7171
ayatsuki-meowmeow <aya.ayatsuki@gmail.com>
2026-05-19 13:39:45 +0900

    refactor: zodスキーマの管理を整理

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,8 +1,1 @@
-import { z } from 'zod'
-
-export const createUserSchema = z.object({
-  name: z.string().min(1).max(255),
-  email: z.string().email().max(255),
-})
-
-export type CreateUserInput = z.infer<typeof createUserSchema>
+export * from './user'
